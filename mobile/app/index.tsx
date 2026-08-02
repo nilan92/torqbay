2d4bf312644ba15e43c368f2cd3af7e143ad2350
Nilan Iddawela <nilan92@gmail.com>
2026-08-02 23:43:22 +0530

    feat(mobile): add auth routing guard to the root layout

--- mobile/app/index.tsx
+++
@@ -1,9 +0,0 @@
-import { ScrollView, Text } from "react-native";
-
-export default function Index() {
-  return (
-    <ScrollView contentInsetAdjustmentBehavior="automatic" contentContainerStyle={{ padding: 16 }}>
-      <Text>Torqbay</Text>
-    </ScrollView>
-  );
-}
